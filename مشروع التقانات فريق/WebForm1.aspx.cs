323baefa08d87b14abdaaa3f5f7c80781425fd04
abdaldaye <abdaldaye@gmail.com>
2020-12-04 10:23:46 +0200

    Add class

---
+++ مشروع التقانات فريق/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using مشروع_التقانات_فريق.tcc_classes;
+
+namespace مشروع_التقانات_فريق
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable td = new DataTable();
+            AcessSqlData acs = new AcessSqlData();
+            acs.open_connection();
+            td = acs.SelectData("select * from student");
+            GridView1.DataSource = td;
+            GridView1.DataBind();
+            acs.close_connection();
+        }
+    }
+}
---
+++ مشروع التقانات فريق/tcc_classes/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace مشروع_التقانات_فريق.tcc_classes
+{
+    public class AcessSqlData
+    {
+        string constring ;
+        SqlConnection connection ;
+        public AcessSqlData()
+        {
+            constring = ConfigurationManager.ConnectionStrings["tcc_con"].ConnectionString;
+            connection = new SqlConnection(constring);
+        }
+        public void open_connection()
+        {
+            connection.Open();
+        }
+        public DataTable SelectData(string Query)
+        {
+            SqlCommand sqlcmd = new SqlCommand(Query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlcmd);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+        public int exR( string Query)
+        {       
+                SqlCommand sqlcmd = new SqlCommand(Query, connection);
+                return sqlcmd.ExecuteNonQuery();
+        }
+        public void close_connection()
+        {
+            connection.Close();
+        }
+    }
+}
